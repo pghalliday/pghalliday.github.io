1095b1b7053d3c5af1fe095f09603126725ecdf7
Peter Halliday <pghalliday@gmail.com>
2014-09-18 17:49:08 +0200

    want to see what travis thinks is the git url

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,10 @@ ENCRYPTED_IV=$encrypted_e5350353280d_iv
 PULL_REQUEST=${TRAVIS_PULL_REQUEST:-false}
 BRANCH=${TRAVIS_BRANCH:-deploy}
 
+# just checking
+REPO=$(git config remote.origin.url)
+echo $REPO
+
 if [ ! -d "$SITE_DIR" ]; then
   echo "SITE_DIR ($SITE_DIR) does not exist, build the site directory before deploying"
   exit 1
@@ -27,6 +31,7 @@ fi
 
 if [ "$BRANCH" == "$DEPLOY_BRANCH" ]; then
   if [ "$PULL_REQUEST" == "false" ]; then
+    REPO=$(git config remote.origin.url)
     if [ -n "$ENCRYPTED_KEY" ]; then
       openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out id_rsa -d
       chmod 600 id_rsa
@@ -34,7 +39,6 @@ if [ "$BRANCH" == "$DEPLOY_BRANCH" ]; then
     fi
     DIR=$(mktemp -d /tmp/pghalliday.github.io.XXXX)
     REV=$(git rev-parse HEAD)
-    REPO=$(git config remote.origin.url)
     git clone --branch ${TARGET_BRANCH} ${REPO} ${DIR}
     rsync -rt --delete --exclude=".git" --exclude=".nojekyll" --exclude=".travis.yml" $SITE_DIR/ $DIR/
     cd $DIR
