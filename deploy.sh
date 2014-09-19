2beaf379a41cb2a04926387f41933d69e5d93427
Peter Halliday <pghalliday@gmail.com>
2014-09-19 13:13:54 +0200

    now using gulp for building and watching

--- deploy.sh
+++ deploy.sh
@@ -34,11 +34,11 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
   echo GIT_EMAIL: $GIT_EMAIL
   if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
-    exit 1
+    exit 0
   else
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
       echo "Travis should not deploy from pull requests"
-      exit 1
+      exit 0
     else
       ENCRYPTED_KEY_VAR=encrypted_${ENCRYPTION_LABEL}_key
       ENCRYPTED_IV_VAR=encrypted_${ENCRYPTION_LABEL}_iv
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,44 @@
 var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
+var spawn = require('child_process').spawn;
+var express = require('express');
+var path = require('path');
+
+var port = process.env.PORT || 4001;
 
 gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('build', function() {
-  // place code for your build task here
+gulp.task('jekyll', ['default'], function() {
+  spawn('jekyll', [
+    'build'
+  ], {
+    stdio: 'inherit'
+  });
 });
+
+gulp.task('server', function(next) {
+  var server = express();
+  server.use(express.static(path.resolve('_site'))).listen(port, next);
+});
+
+gulp.task('watch', ['jekyll', 'server'], function() {
+  watch([
+    '_drafts/**/*',
+    '_layouts/**/*',
+    '_posts/**/*',
+    'bower_components/**/*',
+    'css/**/*',
+    'index.html'
+  ], function() {
+      gulp.start('jekyll');
+  });
+
+  watch('_site/**/*', function(files) {
+      return files.pipe(livereload());
+  });
+});
+
+gulp.task('build', ['jekyll']);
